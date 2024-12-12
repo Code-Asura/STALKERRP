6e63c20ccfe7e02b4269e2c0f476b9471a07a514
Александр <102857254+Code-Asura@users.noreply.github.com>
2024-12-13 00:17:30 +0300

    fix 0.1

--- bot/data/config.py
+++ bot/data/config.py
@@ -5,6 +5,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config_class(BaseSettings):
     token: SecretStr
 
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='/.env', env_file_encoding='utf-8')
 
-config = Config_class
+config = Config_class()
